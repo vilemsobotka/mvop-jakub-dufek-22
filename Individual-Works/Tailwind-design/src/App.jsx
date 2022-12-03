3887bbb15ee161d91ed4eb9d35e9911e596231fb
Jakub Dufek <dufekjak@gmail.com>
2022-12-03 17:29:40 +0100

    chleba

---
+++ Individual-Works/Tailwind-design/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import Menu from './Menu'
+
+function App() {
+
+  return <div class="mx-auto">
+    <Menu />
+  </div>
+
+}
+
+export default App
---
+++ Individual-Works/Tailwind-design/src/Menu.jsx
@@ -0,0 +1,8 @@
+import App from "./App";
+
+function Menu(props) {
+    return <div>
+        <h3>chleba</h3>
+    </div>
+}
+export default Menu
