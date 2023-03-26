94af9b9f94f9dc4fd7af4082bfb04308dc599384
Jakub Dufek <dufekjak@gmail.com>
2023-03-26 22:08:44 +0200

    ende

--- Individual-Works/pinec/src/Clubs.jsx
+++ Individual-Works/pinec/src/Clubs.jsx
@@ -1,6 +1,16 @@
-function Clubs(){
-    return(
-        <div>klub</div>
+function Clubs() {
+    return (<div>
+        <h2 className='text-2xl mt-6 font-bold'>KLUBY</h2>
+        <p className='mt-4'>
+            V Praze se nachází několik významných klubů, které se věnují stolnímu tenisu. Mezi největší patří například TJ Sokol Královské Vinohrady, který má bohatou historii sahající až do roku 1927. Klub má několik týmů, které se účastní pražských soutěží, a také organizuje vlastní turnaje a akce.
+
+            Dalším významným pražským klubem stolního tenisu je SK Vysočina, který se nachází na Praze 3. Klub má dlouholetou tradici a v minulosti se mu podařilo získat mnoho titulů v různých soutěžích.
+
+            Mezi další pražské kluby stolního tenisu patří například TJ Spoje Praha, TTC Praha, nebo STU Praha. Tyto kluby se také vyznačují silnými týmy a účastní se významných soutěží.
+
+            Všem těmto kluby je společné, že nabízejí nejen tréninky pro své členy, ale také různé akce a turnaje pro veřejnost, což přispívá k popularizaci stolního tenisu a umožňuje lidem z různých věkových kategorií zlepšovat své základní a pokročilé stolnoteniské dovednosti.
+        </p>
+    </div>
     )
 }
 export default Clubs
