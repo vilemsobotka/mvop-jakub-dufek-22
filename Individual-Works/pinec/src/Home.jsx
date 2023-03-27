77daa388f145ad231ba958818f6df031d9a87595
Vaše Jméno <vas@email.com>
2023-03-27 15:21:18 +0200

    hodina

--- Individual-Works/pinec/src/Home.jsx
+++ Individual-Works/pinec/src/Home.jsx
@@ -1,7 +1,9 @@
-function Home(){
-    return(
-        <div className=' mx-32 mt-16'>
-        <div className='flex items-center'>
+function Home() {
+  return (
+    <div className='flex mt-16'>
+      <div className="w-48"></div>
+      <div>
+        <div className='lg:flex items-center'>
           <div className='mr-16'>
             <h1 className='text-2xl font-bold'>STOLNÍ TENIS</h1>
             <p className='mt-4'>
@@ -11,7 +13,7 @@ function Home(){
               Stolní tenis je populární po celém světě a může být hrán jako rekreační aktivita nebo jako profesionální sport s mezinárodními turnaji a mistrovstvími světa.
             </p>
           </div>
-          <img className='rounded-3xl w-96' src="https://1gr.cz/fotky/idnes/17/012/cl5/ROU68afcc_pansk.jpg" alt="not found" />
+          <img className='mt-4 lg:mt-0 rounded-3xl w-80' src="https://1gr.cz/fotky/idnes/17/012/cl5/ROU68afcc_pansk.jpg" alt="Panský" />
         </div>
         <h2 className='text-2xl font-bold'>STRUČNÁ PRAVIDLA</h2>
         <ul className='list-decimal mt-4'>
@@ -22,12 +24,14 @@ function Home(){
           <li>Každý hráč má povoleno odrážet míček jen jednou předtím, než ho pošle zpět na protivníkovu stranu stolu. </li>
           <li>Body se získávají, když protivník nedokáže odrážet míček na vaši stranu stolu nebo pokud zkomolí servis. </li>
           <li>První hráč, který získá 11 bodů a má alespoň 2 body náskok, vyhrává sadu. Zápas se obvykle hraje na nejlepší z 3 nebo 5 sad. </li>
-          </ul>
-          <p>
-            Kompletní soupis paravidel naeznete
-            <a className='text-blue-500 underline hover:text-red-500' target={'_blank'} href="https://www.ping-pong.cz/files/documents/2979.pdf"> zde</a>
-          </p>
+        </ul>
+        <p>
+          Kompletní soupis paravidel naeznete
+          <a className='text-blue-500 underline hover:text-red-500' target={'_blank'} href="https://www.ping-pong.cz/files/documents/2979.pdf"> zde</a>
+        </p>
       </div>
-    )
+      <div className="w-24"></div>
+    </div>
+  )
 }
 export default Home
